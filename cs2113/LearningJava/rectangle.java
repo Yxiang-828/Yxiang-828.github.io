0983cd95714caa5f543bf9582ca1e71af3fdd646
yx <yx@email.com>
2025-08-26 19:14:37 +0800

    qn5 done

--- cs2113/LearningJava/rectangle.java
+++ cs2113/LearningJava/rectangle.java
@@ -1,10 +1,64 @@
+public class Main {
+    public static class Circle {
+        private int x;
+        private int y;
+        private double radius;
 
-import java.awt.Rectangle;
+        public Circle() {
+            this(0, 0, 0);
+        }
 
-public class rectangle {
+        public Circle(int x, int y, double radius) {
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+        }
+
+        public int getArea() {
+            double area = Math.PI * Math.pow(radius, 2);
+            return (int) area;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public double getRadius() {
+            return radius < 0 ? radius = 0 : radius;
+        }
+
+        public void setX(int x) {
+            this.x = x;
+        }
+
+        public void setY(int y) {
+            this.y = y;
+        }
+
+        public void setRadius(double radius) {
+            this.radius = radius < 0 ? 0 : radius;
+        }
+    }
+
     public static void main(String[] args) {
-        Rectangle r;
-        r = new Rectangle(0, 0, 5, 10);
-        System.out.println(r);
+        Circle c = new Circle(1, 2, 5);
+
+        c.setX(4);
+        c.setY(5);
+        c.setRadius(6);
+        System.out.println("x      : " + c.getX());
+        System.out.println("y      : " + c.getY());
+        System.out.println("radius : " + c.getRadius());
+        System.out.println("area   : " + c.getArea());
+
+        c.setRadius(-5);
+        System.out.println("radius : " + c.getRadius());
+        c = new Circle(1, 1, -4);
+        System.out.println("radius : " + c.getRadius());
+
     }
 }
